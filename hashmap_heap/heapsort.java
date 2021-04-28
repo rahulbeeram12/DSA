4e783301a0080b5ae51a0581746f29372fb74efc
rahulbeeram12 <rahulbeeram123@gmail.com>
2021-04-28 14:51:36 +0530

    update

--- hashmap_heap/heapsort.java
+++ hashmap_heap/heapsort.java
@@ -47,6 +47,6 @@ public class heapsort{
     public static void main(String args[]){
         int arr[] = { 10, 20, 30, -2, -3, -4, 5, 6, 7, 8, 9, 22, 11, 13 };
 
-        heapsort(arr,true);
+        heapSort(arr,true);
     }
 }
